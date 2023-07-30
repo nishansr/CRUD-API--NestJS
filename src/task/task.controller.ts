fca9c65ad2ed26196a828a35fadb18e2a0b63823
Nishan Bishwokarma <nishanbishokarma98@gmail.com>
2023-07-30 20:45:24 +0545

    update and delete request added

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/task.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -7,9 +7,9 @@ import { PrismaService } from 'src/prisma.service';
 export class TaskController {
   constructor(private readonly taskService: PrismaService) { }
 
-  
+
   @Post()
-  async addTask(@Body() taskBody: CreateTaskDto)  {
+  async addTask(@Body() taskBody: CreateTaskDto) {
     await this.taskService.task.create({
       data: taskBody,
     });
@@ -25,11 +25,40 @@ export class TaskController {
   @Get(':taskName')
   async searchTask(@Param('taskName') taskName: string) {
     const value = await this.taskService.task.findFirst({
-      where:{
-        taskName:taskName
+      where: {
+        taskName: taskName
       }
     })
     return value;
     // return this.arr.filter((task) => (task.taskName = taskName));
   }
+
+  @Get(':id')
+  async updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() task: CreateTaskDto,
+  ) {
+    return await this.taskService.task.update({
+      where: {
+        id: id,
+      },
+      data: {
+        taskName: task.taskName,
+        taskStatus: task.taskStatus,
+      }
+    })
+  }
+
+  @Delete(':id')
+  async deleteTask(
+    @Param('id', ParseIntPipe) id: number
+  ){
+    const num = "213"
+    return await this.taskService.task.delete({
+      where:{
+        id: id
+      }
+    })
+  }
+
 }
